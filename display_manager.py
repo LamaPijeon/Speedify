54e472cae1462f5dc21bd4c5e678027dfe8f2940
Patryk <pjbianca.com@gmail.com>
2026-06-04 21:20:56 -0400

    epd change

--- display_manager.py
+++ display_manager.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw, ImageFont, ImageFilter, ImageOps
 import requests
 import random
 from io import BytesIO
-from waveshare_epd import epd3in6e  # type: ignore
+from epaper import epaper
 
 
 class DisplayManager:
@@ -112,7 +112,7 @@ class DisplayManager:
         epd.display(epd.getbuffer(image))
 
     def epd_init(self):
-        epd = epd3in6e.EPD()
+        epd = epaper('epd3in6e').EPD()
         epd.init()
         epd.Clear()
         return epd
