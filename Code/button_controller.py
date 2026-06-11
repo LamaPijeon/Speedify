ce31c3269a1647fc45568e98d3085cf7253726f5
Patryk <pjbianca.com@gmail.com>
2026-06-11 18:23:01 -0400

    double used button

--- Code/button_controller.py
+++ Code/button_controller.py
@@ -3,7 +3,7 @@ from spotify_client import TrackFetcher
 
 
 class ButtonController:
-    def __init__(self, fetcher: TrackFetcher, prev_pin=17, pause_pin=27, skip_pin=22):
+    def __init__(self, fetcher: TrackFetcher, prev_pin=17, pause_pin=27, skip_pin=4):
         self.fetcher = fetcher
 
         self.prev_btn = Button(prev_pin, pull_up=True)
