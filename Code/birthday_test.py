0e361f02c124439a0d68e611194717d3b8851350
Patryk <pjbianca.com@gmail.com>
2026-06-12 17:26:02 -0400

    wrong again before

--- Code/birthday_test.py
+++ Code/birthday_test.py
@@ -5,6 +5,5 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 
 display = DisplayManager()
-assets_dir = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), "..", "Pics", "Assets")
-display.render_initial(os.path.join(assets_dir, "happy-birthday.png"))
+display.render_initial(os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "..", "Assets", "Pics", "happy-birthday.png"))
