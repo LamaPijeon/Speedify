3d20975f0804b7f38a26f9721cf487f0fb3f4502
Patryk <pjbianca.com@gmail.com>
2026-06-12 15:11:03 -0400

    birthday-test.py

---
+++ Code/birthday_test.py
@@ -0,0 +1,10 @@
+from display_manager import DisplayManager
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+
+display = DisplayManager()
+assets_dir = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "..", "Assets")
+display.render_initial(os.path.join(assets_dir, "happy_birthday.png"))
