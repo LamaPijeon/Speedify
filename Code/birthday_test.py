13ac893d794265fdf22145148dc1417b7a1f0868
Patryk <pjbianca.com@gmail.com>
2026-06-12 17:20:24 -0400

    birthday typo

--- Code/birthday_test.py
+++ Code/birthday_test.py
@@ -7,4 +7,4 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 display = DisplayManager()
 assets_dir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "..", "Assets")
-display.render_initial(os.path.join(assets_dir, "happy_birthday.png"))
+display.render_initial(os.path.join(assets_dir, "happy-birthday.png"))
